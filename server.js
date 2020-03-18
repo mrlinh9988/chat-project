87bed452205f4c8701a62f633281933cd8f349d9
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2020-03-18 20:20:50 +0700

    init hellworld

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const hostname = 'localhost';
+
+app.get('/alo', (req, res) => {
+    res.send('<h1>alo</h1>')
+});
+
+app.listen(port, hostname, () => console.log(`Server start ${hostname}:${port}/`));
+
