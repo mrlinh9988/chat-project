a1eec4490e190a52e4ec400f4462a03a6ab11171
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2020-03-19 16:10:35 +0700

    [1.0.0] Connect to MongoDB

---
+++ sh/env.example.sh
@@ -0,0 +1,11 @@
+  # Config datatbase environment variables
+  export DB_CONNECTION=***
+  export DB_HOST=***
+  export DB_PORT=***
+  export DB_NAME=***
+  export DB_USERNAME=***
+  export DB_PASSWORD=***
+
+  # config app environment variables
+  export APP_PORT=***
+  export APP_HOST=*** 
