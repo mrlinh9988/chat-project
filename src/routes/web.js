a98bb836d51e15cbafe13c2bd5bd75c108bef5a2
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2020-03-20 01:31:33 +0700

    [1.0.0] register user part 1

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { home, auth } from "../controllers/index";
+import authValid from "../validation/authValidation";
 
 let router = express.Router();
 
@@ -11,9 +12,12 @@ let router = express.Router();
 let initRoutes = (app) => {
   router.get('/', home.getHome);
   router.get('/login-register', auth.getLoginRegister);
- 
+  router.post('/register', authValid.register, auth.postRegister);
+
   return app.use('/', router);
 }
 
 module.exports = initRoutes;
+
+
 
